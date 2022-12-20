dd10bfb9668bbfa17d368537ca15175d9ef54885
Chappydev <danjtchapman@gmail.com>
2022-12-19 21:58:51 -0700

    Add options shade to child-nodes

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Check, Edit3, X } from 'react-feather';
 import { NavLink } from 'react-router-dom';
 import useAnimation from '../hooks/useAnimation';
 import useRect from '../hooks/useRect';
@@ -12,10 +13,10 @@ const Node = ({
   isFirst,
   parentRect = null,
   setRect = null,
-  delayMult = 0,
-  children
+  delayMult = 0
 }) => {
   const [sizeObj, setSizeObj] = useState({});
+  const [isShown, setIsShown] = useState(false);
   const delay = delayMult * 1000 - 300 < 0 ? 0 : delayMult * 1000 - 300;
   const colorAnimation = useAnimation('easeInOutExpo', 1000, delay);
 
@@ -37,44 +38,54 @@ const Node = ({
     }
   }, [node.isComplete, colorAnimation]);
 
+  // TODO: Add options shade to first-node
   if (isFirst) {
     return (
-      <NavLink
-        to={`/task/${node.id}`}
-        className="first-node node"
-        ref={currNodeRef}
-      >
+      <div className="first-node node-container">
+        <NavLink to={`/task/${node.id}`} className="node" ref={currNodeRef}>
+          <div className="hidden-relative-box">
+            <div className="complete-color" style={sizeObj}></div>
+            {node.name}
+          </div>
+        </NavLink>
+      </div>
+    );
+  }
+  return (
+    <div
+      className="child-node node-container"
+      onMouseOver={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      {/* 
+        TODO: Keep options shade from closing when hovering
+              over it.
+      */}
+      <div className={`node-options${isShown ? ' display' : ''}`}>
+        <Check className="check option" />
+        <Edit3 className="edit option" />
+        <X className="x option" />
+      </div>
+      <NavLink to={`/task/${node.id}`} className="node" ref={currNodeRef}>
         <div className="hidden-relative-box">
           <div className="complete-color" style={sizeObj}></div>
           {node.name}
         </div>
+        {currNodeRef &&
+        currNodeRef.current &&
+        parentRect &&
+        Object.keys(parentRect).length !== 0 ? (
+          <Line
+            x1={parentRect.xCenter}
+            y1={parentRect.yCenter}
+            x2={rect.xCenter}
+            y2={rect.yCenter}
+            delayMult={delayMult + 1}
+            isComplete={node.isComplete}
+          />
+        ) : null}
       </NavLink>
-    );
-  }
-  return (
-    <NavLink
-      to={`/task/${node.id}`}
-      className="child-node node"
-      ref={currNodeRef}
-    >
-      <div className="hidden-relative-box">
-        <div className="complete-color" style={sizeObj}></div>
-        {node.name}
-      </div>
-      {currNodeRef &&
-      currNodeRef.current &&
-      parentRect &&
-      Object.keys(parentRect).length !== 0 ? (
-        <Line
-          x1={parentRect.xCenter}
-          y1={parentRect.yCenter}
-          x2={rect.xCenter}
-          y2={rect.yCenter}
-          delayMult={delayMult + 1}
-          isComplete={node.isComplete}
-        />
-      ) : null}
-    </NavLink>
+    </div>
   );
 };
 export default Node;
