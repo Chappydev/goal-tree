26bee2efd5a742219f1ca0accc0bc2b32eb03c2e
Chappydev <danjtchapman@gmail.com>
2023-01-17 22:39:23 -0700

    Add customized node and styles

--- src/components/D3Tree.jsx
+++ src/components/D3Tree.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
 import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 import useCenteredTree from '../hooks/useCenteredTree';
 import queryFunctions from '../utility/queryFunctions';
+import './D3Tree.scss';
+
+const renderNodeWithCustomEvents = ({ nodeDatum, toggleNode, handleClick }) => {
+  return (
+    <g>
+      <circle
+        className={`${nodeDatum.isComplete ? 'complete ' : ''}node-circle`}
+        r="15"
+        onClick={() => handleClick(nodeDatum)}
+      />
+      <text fill="black" strokeWidth="1" x="20" onClick={toggleNode}>
+        {nodeDatum.name}
+      </text>
+    </g>
+  );
+};
 
 const D3Tree = () => {
   const {
@@ -14,7 +31,14 @@ const D3Tree = () => {
     queryFn: queryFunctions.findGoal
   });
 
+  const navigate = useNavigate();
+
+  // TODO: mess with the translation to look good visually
   const [translate, containerRef] = useCenteredTree();
+
+  const handleClick = (nodeDatum) => {
+    navigate(`/task/${nodeDatum.id}`);
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -34,6 +58,9 @@ const D3Tree = () => {
         data={tree}
         translate={translate}
         pathFunc="straight"
+        renderCustomNodeElement={(rd3tProps) =>
+          renderNodeWithCustomEvents({ ...rd3tProps, handleClick })
+        }
         orientation="vertical"
       />
     </div>
