2b18fbfd9d8eff05a3d07133a913fd048a86ac1e
Chappydev <danjtchapman@gmail.com>
2023-02-11 13:06:13 -0700

    Create debounce value hook

---
+++ src/hooks/useDebounceValue.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+const useDebounceValue = (value, delay = 500) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+export default useDebounceValue;
