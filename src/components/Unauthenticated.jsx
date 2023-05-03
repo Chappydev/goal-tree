66027e95475f18591ef2a3b4ec80c7719b95202c
Chappydev <danjtchapman@gmail.com>
2023-05-02 21:42:47 -0600

    Create 'landing page' for unauthenticated users

--- src/components/Unauthenticated.jsx
+++ src/components/Unauthenticated.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from './Button';
 import { loginSignupFormIsShownAtom, signUpLoginAtom } from './Navbar';
 import './Unauthenticated.scss';
+import { ArrowRight } from 'react-feather';
 
 const Unauthenticated = () => {
   const setUseSignUp = useSetAtom(signUpLoginAtom);
@@ -12,30 +13,22 @@ const Unauthenticated = () => {
 
   return (
     <div className="unauthenticated-container">
-      <p className="unauthenticated-content">
-        Unauthenticated:{' '}
-        <Button
-          onClick={() => {
-            setUseSignUp(false);
-            openLoginModal();
-          }}
-          fillType="fill"
-          color="primary"
-        >
-          Login
-        </Button>{' '}
-        or{' '}
-        <Button
-          onClick={() => {
-            setUseSignUp(true);
-            openLoginModal();
-          }}
-          fillType="fill"
-          color="accent"
-        >
-          Sign Up
-        </Button>
-      </p>
+      <h1>Welcome to GoalTree!</h1>
+      <h2>A more intuitive way to manage goals, tasks, and much more.</h2>
+      <p className='unauthenticated-description'>One major reason for procrastination is simply that our tasks are often too overwhelming to even get started on them. That's why GoalTree! uses a tree structure to organize your tasks more intuitively. Break down your tasks into smaller and smaller pieces, and finally gain the confidence to get started.</p>
+      <h2>Get Started!</h2>
+      <div className='unauthenticated-getstarted'>
+        <div className='getstarted-option'>
+          <h4>Demo</h4>
+          <p>Try out the app without an account.</p>
+          <ArrowRight />
+        </div>
+        <div className="getstarted-option unauthenticated-content">
+          <h4>Create an Account</h4>
+          <p>Create an account to start saving your work for next time.</p>
+          <ArrowRight />
+        </div>
+      </div>
     </div>
   );
 };
