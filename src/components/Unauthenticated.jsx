f9c266da9455b9cf6321e8885fff73f5d12419d8
Chappydev <danjtchapman@gmail.com>
2023-05-05 18:56:51 -0600

    Finish styling for the getstarted options

--- src/components/Unauthenticated.jsx
+++ src/components/Unauthenticated.jsx
@@ -1,9 +1,9 @@
 import { useSetAtom } from 'jotai';
 import React from 'react';
-import Button from './Button';
 import { loginSignupFormIsShownAtom, signUpLoginAtom } from './Navbar';
 import './Unauthenticated.scss';
 import { ArrowRight } from 'react-feather';
+import { NavLink } from 'react-router-dom';
 
 const Unauthenticated = () => {
   const setUseSignUp = useSetAtom(signUpLoginAtom);
@@ -18,15 +18,23 @@ const Unauthenticated = () => {
       <p className='unauthenticated-description'>One major reason for procrastination is simply that our tasks are often too overwhelming to even get started on them. That's why GoalTree! uses a tree structure to organize your tasks more intuitively. Break down your tasks into smaller and smaller pieces, and finally gain the confidence to get started.</p>
       <h2>Get Started!</h2>
       <div className='unauthenticated-getstarted'>
-        <div className='getstarted-option'>
-          <h4>Demo</h4>
-          <p>Try out the app without an account.</p>
-          <ArrowRight />
+        <div className="getstarted-option-wrapper">
+          <NavLink to="/demo" className="unauthenticated-demo-link getstarted-option transparent-text-transition">
+            <div>
+              <h4>Demo</h4>
+              <p>Try out the app without an account.</p>
+            </div>
+            <ArrowRight />
+          </NavLink>
         </div>
-        <div className="getstarted-option unauthenticated-content">
-          <h4>Create an Account</h4>
-          <p>Create an account to start saving your work for next time.</p>
-          <ArrowRight />
+        <div className="getstarted-option-wrapper">
+          <button type="button" className='getstarted-option transparent-text-transition'>
+            <div>
+              <h4>Create an Account</h4>
+              <p>Create an account to start saving your work for next time.</p>
+            </div>
+            <ArrowRight />
+          </button>
         </div>
       </div>
     </div>
